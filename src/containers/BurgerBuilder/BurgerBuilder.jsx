22cdefc2c62adc832c7818a71acb5f5a2aa25536
Amit Samanta <developer.amitsamanta@gmail.com>
2020-11-16 12:53:03 +0530

    Add the components and containers for the burger app

---
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends React.Component {
+    render () {
+        return (
+            <React.Fragment>
+                <Burger />
+                <div>Build Controls</div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default BurgerBuilder;
