de45835b1fbdf005d00104e41a4268302497428c
Amit Samanta <developer.amitsamanta@gmail.com>
2021-01-06 11:41:59 +0530

    Modify the saga with the new action methods

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,6 +1,6 @@
 import { put } from 'redux-saga/effects';
 
-import * as actionTypes from '../actions/actionTypes';
+import * as actions from '../actions/auth';
 
 // generators
 export function* logoutSaga (action) {
@@ -8,7 +8,5 @@ export function* logoutSaga (action) {
     yield localStorage.removeItem('expirationDate');
     yield localStorage.removeItem('userId');
 
-    yield put ({
-        type: actionTypes.AUTH_LOGOUT
-    });
+    yield put (actions.authLogout());
 }
