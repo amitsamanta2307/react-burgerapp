193ca51fd100fb9e8e540e299714b4cc8b82bc8f
Amit Samanta <developer.amitsamanta@gmail.com>
2021-01-06 12:32:20 +0530

    add the authCheckState saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -4,11 +4,13 @@ import * as actionTypes from '../actions/actionTypes';
 import {
     logoutSaga,
     checkAuthTimeoutSaga,
-    authUserSaga
+    authUserSaga,
+    authCheckStateSaga
 } from './auth';
 
 export function* watchAuth() {
+    yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
     yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
-    yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
+    yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
 }
